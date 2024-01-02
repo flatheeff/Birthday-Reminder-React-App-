d161718034884031ed01b6e0411976e7f4410e7d
Joyna-Jose <joynajose2006@gmail.com>
2024-01-02 11:20:26 -0800

    Supabase created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -78,7 +78,7 @@ const Form = () => {
                                         required=""
                                         {...register("employee_img")} />
                                 </div>
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border-2 border-white">Register</button>
+                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border-2 border-black text-black">Register</button>
 
                             </form>
                         </div>
