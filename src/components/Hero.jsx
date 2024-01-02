0a98ad09c860b5ec354d9e8f617346eac864d5a9
flatheeff <flatheef@gmail.com>
2024-01-02 12:12:37 +0530

    create footer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <div>
 
-        <div className="flex flex-col text-center p-36 text-black  justify-center gap-10">
-            <h1 className="md:text-4xl text-purple-500 ">BIRTHDAY</h1>
-            <p className="md:text-3xl text-white ">All the best wishes for my best friend! I hope you're enjoying your special day doing whatever it is you want! Wowee, another loop around the sun. HAPPY ...</p>
-            <button className='md:text-2xl border-2 font-bold text-white'>Find Birthday</button>
+        <div className="flex flex-col text-center p-36 text-black  justify-center items -center gap-10">
+            <h1 className="md:text-5xl text-purple-500 font-bold italic ">BIRTHDAY</h1>
+            <p className="md:text-3xl text-white text-white italic">All the best wishes for my best friend! I hope you're enjoying your special day doing whatever it is you want! Wowee, another loop around the sun. HAPPY BIRTHDAY</p>
+            <button className='md:text-2xl w-32  border-2 p-3 rounded-xl font-bold text-purple-500 '>Find Birthday</button>
             
             </div>
         </div>
