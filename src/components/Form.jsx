4d37887b29cf34bd895ce6526216f6545436ef1f
Joyna-Jose <joynajose2006@gmail.com>
2024-01-04 14:25:25 -0800

    background created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -27,8 +27,8 @@ const Form = () => {
     return (
         <div>
 
-            <section class="bg-black dark:bg-gray-900">
-                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <section class="dark:bg-gray-900">
+                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
 
                     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -78,7 +78,7 @@ const Form = () => {
                                         required=""
                                         {...register("employee_img")} />
                                 </div>
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border-2 border-black text-black">Register</button>
+                                <button type="submit" class="w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border-2 border-black text-black">Register</button>
 
                             </form>
                         </div>
